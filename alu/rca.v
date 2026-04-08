c477dcbf154eea7ef645fa0f9035e211af75a897
mary <165078745+maryisafangirl@users.noreply.github.com>
2026-04-09 01:11:28 +0300

    comentarii adunare

--- alu/rca.v
+++ alu/rca.v
@@ -2,23 +2,24 @@ module fac(
   input a, b, cin, 
   output sum, cout
   );
+  //formulele obtinute din simplificarea cu harti Karnaugh
   assign sum=(a^b^cin);
   assign cout=((a&b)|(b&cin)|(a&cin));
 endmodule
 
-module rca#(parameter w=8)(
-  input [w-1:0]a,
+module rca#(parameter w=8)( //modul parametrizabil
+  input [w-1:0]a, //a si b sunt termenii sumei
   input [w-1:0]b,
-  input cin,
-  output [w-1:0]sum,
-  output cout
+  input cin, //carry in (transportul de intrare)
+  output [w-1:0]sum, //suma
+  output cout //carry out (transportul de iesire)
   );
-  wire [w:0]c_aux;
+  wire [w:0]c_aux; //conecteaza cout-ul unei celule la cin-ul urmatoarei
   assign c_aux[0]=cin;
   genvar i;
   generate
   for(i=0;i<w;i=i+1) begin: v
-    fac f1(.a(a[i]),.b(b[i]),.cin(c_aux[i]),.sum(sum[i]),.cout(c_aux[i+1]));
+    fac f1(.a(a[i]),.b(b[i]),.cin(c_aux[i]),.sum(sum[i]),.cout(c_aux[i+1])); //se calculeaza suma bit cu bit folosind modulul full adder cell de w ori
   end
   endgenerate
   assign cout=c_aux[w];
